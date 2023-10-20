e9bc9f17474e2a0dfdd31e466442a02b42e267d0
Alysson <limalysson@gmail.com>
2023-10-20 16:00:58 -0300

    Soluções melhoradas

--- atividades/script.js
+++ atividades/script.js
@@ -1,34 +1,51 @@
 function ola(nome, sobrenome, idade){
-    var nome
-    var sobrenome
-    var idade
-    console.log("Olá, meu nome é "+nome+" "+sobrenome+", tenho "+idade+" anos.")
+    console.log(`Olá, meu nome é ${nome} ${sobrenome}, tenho ${idade} anos.`)
 
 }
-ola("Alysson", 'Lima', 33)
+ola("Alysson", "Lima", 33)
 
-function soma(a, b){
-    c = a + b
-    console.log(c)
-}
-soma(3,4)
+// ----------------------------------------------------
 
-function mIdade(a){
-    if (a<18){
-        console.log("Você é menor de idade.")
-    } else {
-        console.log("Você é maior de idade")
-    }
-}
-mIdade(17)
-mIdade(19)
+// function soma(){
+//     num3 = num1 + num2
+//     console.log(num3)
+//     alert(`O resultado da soma é ${num3}`)
+//     console.log(`O resultado da soma é ${num3}`)
+// }
+// var num1 = Number(window.prompt("Digite o 1º numero:", ""));
+// var num2 = Number(window.prompt("Digite o 2º numero:", ""));
+// soma()
 
-function cont100(a){
-    a = 0
-    while (a < 100) {
-        console.log(a)
-        a = a + 1
-    }
-    console.log(a)
-}
-cont100(100)
+// ------------------------------------
+
+// let dataAtual = new Date();
+
+// function calculaIdade(idade)
+// {
+//   let anoAtual = dataAtual.getFullYear();
+//   let idadeCalculada = anoAtual - idade;
+//   if (idadeCalculada < 18){
+//     console.log(`Você tem ${idadeCalculada} anos, logo você é menor de idade.`)
+//     alert(`Você tem ${idadeCalculada} anos, logo você é menor de idade.`)
+//     } else {
+//     console.log(`Você tem ${idadeCalculada} anos, logo você é maior de idade.`)
+//     alert(`Você tem ${idadeCalculada} anos, logo você é maior de idade.`)
+//     }
+//   }
+
+// var idadeDigitada = window.prompt("Digite seu ano de nascimento!");
+// calculaIdade(idadeDigitada)
+
+// -----------------------------------------------
+
+// function cont(){
+//     numInit = 0
+//     while (numInit < numFim) {
+//         console.log(numInit)
+//         numInit = numInit + 1
+//     }
+//     console.log(numInit)
+// }
+// var numFim = Number(window.prompt("Até qual número você quer que eu exiba?"))
+// cont()
+
